8b73b060f0637a351d5ffefe09f5ebc7b215733b
nemu626 <yamada0626@gmail.com>
2024-07-22 10:39:21 +0000

    change regex test to switch by codePoint

--- src/util.ts
+++ src/util.ts
@@ -8,8 +8,18 @@ export const countTextLength = (str: string): number =>
 		.reduce((acc, cur): number => acc + (isFullWidthChar(cur) ? 2 : 1), 0);
 
 export const isFullWidthChar = (char: string): boolean => {
-	const halfWidthRegExp = /[\x01-\x7E\u{FF65}-\u{FF9F}]/mu;
-	return !halfWidthRegExp.test(char);
+	const codePoint = char.codePointAt(0);
+	if (codePoint === undefined) return false;
+
+	return (
+		(codePoint >= 0xff01 && codePoint <= 0xff60) ||
+		(codePoint >= 0xffe0 && codePoint <= 0xffe6) ||
+		(codePoint >= 0x3000 && codePoint <= 0x303f) || // ZENKAKU PUNCTUATION
+		(codePoint >= 0x3040 && codePoint <= 0x309f) || // HIRAGANA
+		(codePoint >= 0x30a0 && codePoint <= 0x30ff) || // KATAKANA
+		(codePoint >= 0x4e00 && codePoint <= 0x9fff) || // KANJI
+		(codePoint >= 0xac00 && codePoint <= 0xd7af) // HANGUL
+	);
 };
 
 export const padEndFullWidthChar = (
